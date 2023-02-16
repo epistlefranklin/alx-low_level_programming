39b7220f3a495ae6669c22482334ea3055b8a1ed
epistlefranklin <franklinephranquelyn@gmail.com>
2023-02-16 15:17:50 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is a basic c program
+ *
+ * Return: return 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
